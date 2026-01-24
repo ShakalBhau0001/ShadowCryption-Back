5d52c3fdad1fa72749bcb5d0dd923b705e0c1b34
Shakalen Shaikh <160219957+ShakalBhau0001@users.noreply.github.com>
2026-01-24 18:43:15 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,1 +1,7 @@
+from fastapi import FastAPI
+from .routers import stego_image, stego_audio, contact
 
+app = FastAPI(title="ShadowCryption API")
+app.include_router(stego_image.router)
+app.include_router(stego_audio.router)
+app.include_router(contact.router)
